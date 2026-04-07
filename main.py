4856b67a31b671f5d687fa955666804b7f3cd192
karentrasporte <karentrasporte@gmail.com>
2026-04-07 11:14:13 +0800

    Created functions to scrape data from URL. Send email function is still dummy

--- main.py
+++ main.py
@@ -17,7 +17,27 @@ def extract(source):
     return value
 
 
+def send_email():
+    print("Send email...")
+
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+
 if __name__ == "__main__":
     scrapped = scrape(URL)
     extracted = extract(scrapped)
     print(extracted)
+    content = read(extracted)
+    if extracted != "No upcoming tours":
+        if extracted not in content:
+            store(extracted)
+            send_email()
+
